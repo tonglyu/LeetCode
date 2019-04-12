79a462558e447937f0225ce1cc4885193b19733b
tonglyu <tonglyu9647@gmail.com>
2019-04-12 01:23:39 -0700

    91. Decode Ways

---
+++ Medium/LC091.java
@@ -0,0 +1,69 @@
+package Leetcode.Medium;
+
+/**
+ * 91. Decode Ways
+ * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * Given a non-empty string containing only digits, determine the total number of ways to decode it.
+ *
+ * Example 1:
+ * Input: "12"
+ * Output: 2
+ * Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+ *
+ * Example 2:
+ * Input: "226"
+ * Output: 3
+ * Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+ */
+public class LC091 {
+	/**
+	 * Data structure: dp
+	 * dp[0] = 1;
+	 * dp[1] = str[0] == '0' ? 0 : 1;
+	 * dp[i] = dp[i-1] + dp[i-2];
+	 *
+	 * Time = O(n)
+	 * Space = O(n) which can be optimized to O(1)
+	 */
+	public int numDecodings(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+
+		int[] dp = new int[s.length() + 1];
+		dp[0] = 1;
+		dp[1] = s.charAt(0) == '0' ? 0 : 1;
+		for (int i = 2; i <= s.length(); i++) {
+			dp[i] = 0;
+			int lastOne = s.charAt(i - 1) - '0';
+			if (lastOne >= 1 && lastOne <= 9) {
+				dp[i] += dp[i - 1];
+			}
+			int lastTwo = Integer.valueOf(s.substring(i - 2, i));
+			if (lastTwo >= 10 && lastTwo <= 26) {
+				dp[i] += dp[i - 2];
+			}
+		}
+		return dp[s.length()];
+	}
+
+	public static void main(String[] args) {
+		LC091 sol = new LC091();
+		String[] str = new String[]{"0","01","10","12","226"};
+		for (String s:str) {
+			System.out.println(sol.numDecodings(s));
+		}
+	}
+
+	// 0 1 2 3 4
+	// 1 2 2 3 4
+	//dp[1] = 1                     i = 1   k = 0
+	//dp[2] = 1 + dp[1] = 2         i = 2,  k = 0,1
+	//dp[3] = dp[1] + d[2] = 3      i = 3,  k = 1,2
+	//dp[4] = dp[2] + dp[3] = 5
+	//dp[5] = dp[4] = 5
+}
