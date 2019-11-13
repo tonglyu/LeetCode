5b91d490da0a2d715017daf5d2ff17e52e270dbc
tonglyu <tonglyu9647@gmail.com>
2019-11-12 20:04:10 -0800

    496. Next Greater Element I

--- Easy/LC0496.java
+++ Easy/LC0496.java
@@ -45,13 +45,13 @@ public class LC0496 {
 	 */
 	public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 		Map<Integer, Integer> map = new HashMap<>();
-		Deque<Integer> s = new LinkedList<>();
+		Deque<Integer> stack = new LinkedList<>();
 		for (int i = nums2.length - 1; i >= 0; i--) {
-			while (!s.isEmpty() && s.peek() <= nums2[i]) {
-				s.pop();
+			while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+				stack.pop();
 			}
-			map.put(nums2[i], s.isEmpty() ? -1 : s.peek());
-			s.push(nums2[i]);
+			map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+			stack.push(nums2[i]);
 		}
 
 		int[] res = new int[nums1.length];
@@ -73,13 +73,13 @@ public class LC0496 {
 	 */
 	public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
 		Map<Integer, Integer> map = new HashMap<>();
-		Deque<Integer> s = new LinkedList<>();
+		Deque<Integer> stack = new LinkedList<>();
 
 		for (int i = 0; i < nums2.length; i++) {
-			while (!s.isEmpty() && s.peek() < nums2[i]) {
-				map.put(s.pop(), nums2[i]);
+			while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+				map.put(stack.pop(), nums2[i]);
 			}
-			s.push(nums2[i]);
+			stack.push(nums2[i]);
 		}
 
 		int[] res = new int[nums1.length];
