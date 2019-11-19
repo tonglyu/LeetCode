a317df14b3c22656d557f93560029a7e49e285d1
tonglyu <tonglyu9647@gmail.com>
2019-11-18 16:02:17 -0800

    140. Word Break II

--- Hard/LC0140.java
+++ Hard/LC0140.java
@@ -1,5 +1,9 @@
 package Leetcode.Hard;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+
 import java.util.*;
 
 /**
@@ -41,6 +45,9 @@ import java.util.*;
 public class LC0140 {
 	/**
 	 * Method1: DP, but TLE?
+	 * dp[i] represents the list of valid combination of substring to form word[0,i-1]
+	 * dp[i] can add current word[j,i) if word in dict and dp[j]
+	 *
 	 * Time = O(n^3)
 	 * Space = O(n^3)
 	 */
@@ -49,22 +56,16 @@ public class LC0140 {
 		if (s.length() == 0) return res;
 
 		Set<String> dict = new HashSet<>(wordDict);
-		List<String>[] dp = new ArrayList[s.length() + 1];
-		dp[0] = new ArrayList<>();
-		dp[0].add("");
+		List<String>[] dp = new List[s.length() + 1];
+
+		dp[0] = new ArrayList<>(Arrays.asList(""));
 		for (int i = 1; i <= s.length(); i++) {
 			List<String> cur = new ArrayList<>();
 			for (int j = 0; j < i; j++) {
 				String sub = s.substring(j,i);
 				if (dp[j].size() > 0 && dict.contains(sub)) {
 					for (String ele: dp[j]) {
-						StringBuilder tmp = new StringBuilder();
-						tmp.append(ele);
-						if (!ele.equals("")) {
-							tmp.append(" ");
-						}
-						tmp.append(sub);
-						cur.add(tmp.toString());
+						cur.add(ele.equals("") ? sub : ele + " " + sub);
 					}
 				}
 			}
@@ -83,15 +84,17 @@ public class LC0140 {
 	 * of string from index  i + 1
 	 *
 	 * n levels, for each level, we need to traverse n - 1/ n- 2/ ... 1 letters, and build a list need O(n) time
-	 * Time = O(n*n*n)
-	 * Space = O(n*n*n)
+	 * Time = O(n^3) Size of recursion tree can go up to n^2. The creation of list takes n time.
+	 * Space = O(n^3) The depth of the recursion tree can go up to n and each activation record can contains a string list of size n.
 	 */
 	public List<String> wordBreak2(String s, List<String> wordDict) {
-		if (s.length() == 0) return new ArrayList<>();
+		if (s.length() == 0) {
+			return new ArrayList<>();
+		}
 		Set<String> dict = new HashSet<>(wordDict);
 		Map<Integer, List<String>> map = new HashMap<>();
 
-		List<String> res = dfs(s, dict,0, map );
+		List<String> res = dfs(s, dict,0, map);
 		return res;
 	}
 
@@ -100,16 +103,17 @@ public class LC0140 {
 			return map.get(start);
 		}
 
-		List<String> res = new ArrayList<>();
-		if (s.length() == start) {
-			res.add("");
+		if (start == s.length()) {
+			return new ArrayList<>(Arrays.asList(""));
 		}
 
+		List<String> res = new ArrayList<>();
 		for (int end = start + 1; end <= s.length(); end++) {
-			if (dict.contains(s.substring(start,end))) {
+			String sub = s.substring(start,end);
+			if (dict.contains(sub)) {
 				List<String> list = dfs(s, dict, end, map);
 				for (String temp: list) {
-					res.add(s.substring(start,end) + (temp.equals("") ? "" : " ") + temp);
+					res.add(temp.equals("") ? sub : sub +  " " + temp);
 				}
 			}
 		}
@@ -118,14 +122,37 @@ public class LC0140 {
 		return res;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void test1() {
 		String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-		String[] wordDict = new String[]{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
+		List<String> wordDict = new ArrayList<>(Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"));
 
-		String s1 = "pineapplepenapple";
-		String[] wordDict1 = new String[]{"apple", "pen", "applepen", "pine", "pineapple"};
-		LC0140 sol = new LC0140();
-		List<String> res = sol.wordBreak2(s,Arrays.asList(wordDict));
-		System.out.println(res);
+		List<String> exp = new ArrayList<>();
+		List<String> act = wordBreak2(s,wordDict);
+		Assert.assertTrue(exp.containsAll(act) && act.containsAll(exp));
+	}
+
+	@Test
+	public void test2() {
+		String s = "catsanddog";
+		List<String> wordDict = new ArrayList<>(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+
+		List<String> exp = new ArrayList<>(Arrays.asList("cats and dog", "cat sand dog"));
+		List<String> act = wordBreak(s,wordDict);
+		Assert.assertTrue(exp.containsAll(act) && act.containsAll(exp));
+	}
+
+	@Test
+	public void test3() {
+		String s = "pineapplepenapple";
+		List<String> wordDict = new ArrayList<>(Arrays.asList("apple", "pen", "applepen", "pine", "pineapple"));
+
+		List<String> exp = new ArrayList<>(Arrays.asList("pine apple pen apple", "pineapple pen apple", "pine applepen apple"));
+		List<String> act = wordBreak2(s,wordDict);
+		Assert.assertTrue(exp.containsAll(act) && act.containsAll(exp));
+	}
+
+	public static void main(String[] args) {
+		JUnitCore.main(LC0140.class.getName());
 	}
 }
